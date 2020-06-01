5a62c940bfb90d99ded9e06322ec698b5517fd89
Alvin <greedyalvin@163.com>
2020-06-01 16:57:37 +0800

    Api gateway & sentinel flow control

---
+++ sample-5/gateway/src/main/java/com/greedystar/gateway/GateApplication.java
@@ -0,0 +1,17 @@
+package com.greedystar.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author GreedyStar
+ * Date   2020-6-1
+ */
+@SpringBootApplication
+public class GateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class);
+    }
+
+}
